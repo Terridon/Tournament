b3b88edf0f6d19baa60496e1d216674dae6c1a7e
Sarah Brofeldt <sarah@antistof.(none)>
2010-03-14 17:57:43 +0100

    deleted:    bin/model/.Participant.class.swp modified:   bin/model/Round.class modified:   src/model/Round.java

--- src/model/Round.java
+++ src/model/Round.java
@@ -3,20 +3,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.*;
 public class Round {
-		private List pairs = new ArrayList();
-private List list = Collections.synchronizedList(new ArrayList(...));
-		private Participant[] participants;		
+	private ArrayList<Pair> pairs = new ArrayList();
+	private List list = Collections.synchronizedList(new ArrayList(...));
+	private Participant[] participants;		
 
 	public Round(Participant[] participants) {
 		this.participants = participants;
 		pairs = new Pair[participants.length / 2];	
 	}	
-	
+
 	private Pair[] pairParticipants() {
 		Collections.shuffle(Arrays.asList(participants));
-			for (int i = 0; i < participants.length; i++) {
-		//	(i + 1) % 2;
-		//i++ skal formodentligt sættes til i+2 for at tage par
+		for (int i = 0; i < participants.length; i++) {
+			//	(i + 1) % 2;
+			//i++ skal formodentligt sættes til i+2 for at tage par
 			Pair pairs = new Pair(participants[i], participants[i+1]);	
 			this.pairs.add(pairs);
 		}
