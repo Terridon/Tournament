110b762519d74b717589c15e13d6f8d97f4bfc8a
Terridon <Dolphspam@gmail.com>
2010-03-16 18:58:35 +0100

    test af push :P

--- src/model/Round.java
+++ src/model/Round.java
@@ -1,7 +1,22 @@
-/bin/bash: :wq: command not found
+package model;
+import java.util.Collections;
+import java.util.*;
+
+public class Round {
+	private List<Pair> pairs;
+	private List<Participant> participants;         
+
+	public Round(List<Participant> participants) {
+		this.participants = participants;
+		pairParticipants();
+	}       
+
+	private void pairParticipants() {
+		Collections.shuffle(participants);
+		for (int i = 0; i < participants.size(); i=i+2) {
+			Pair pair = new Pair(participants.get(i), participants.get(i+1));       
 			this.pairs.add(pair);
 		}
-	}	
+	}       
 }
-
 
