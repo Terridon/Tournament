c4db8195af569614213a256d962ef687919e143f
Terridon <Dolphspam@gmail.com>
2010-03-18 18:23:34 +0100

    Mindre udvidelse til controller

--- src/control/TourTUI.java
+++ src/control/TourTUI.java
@@ -4,28 +4,50 @@ import model.Tournament;
 import model.Participant;
 
 public class TourTUI {
+		private static final int ADDPLAYER = 1;
+		private static final int EXIT = 0;
+		private static final int STARTTOUR = 2;
+		private static final int NO_CHOISE = -1;
 
 	public TourTUI(){
-
 	}
-
+	
 	public void start() {
 		Tournament tour = new Tournament();
-<<<<<<< HEAD:src/control/TourTUI.java
+		int choise = NO_CHOISE;
+		displayMenu();
+		//TODO skal rykkes ned i newplayer
 		Scanner input = new Scanner(System.in);
 		System.out.println("write player-name:");
 		String name = input.nextLine();
 		Participant player = new Participant(name);
 		tour.addParticipant(player);
 		//TODO 
-		//delete-option ved forkert navn
-=======
-		//TODO: userprompt somehow;
-		Participant player = new Participant("Dan");
-		tour.addParticipant(player);	
->>>>>>> 62292a8f7f135eeb0d73679a4121538081c829f3:src/control/TourTUI.java
+		//delete-option ved forkert navn eller change-name-funktion
+	}
+	
+	private void displayMenu() {
+		System.out.println();
+		System.out.println("Enter the number for the action you wish to perform");
+		System.out.println("Add player............." + ADDPLAYER);
+		System.out.println("Start tournament......." + STARTTOUR);		
+		System.out.println("Exit..................." + EXIT);		
+	
 	}
 
+	private void executeChoise(int choise) {
+		System.out.println();
+		if (choise == ADDPLAYER)
+			newPlayer();
+		else if (choise == STARTTOUR)
+			tour.startRound();
+		else if (choise == EXIT)
+			System.out.println("Program has been closed");
+	}
+
+	public void newPlayer() {
+		
+	}
 
 
 }
